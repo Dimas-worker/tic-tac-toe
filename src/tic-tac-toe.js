a46a02c14288a4325a73dee35f9fb2b29c3cd010
Dmitry-NBook <pdo.hardwork@yandex.ru>
2021-08-05 13:59:57 +0300

    feat: add solition according to the task

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,78 @@
 class TicTacToe {
-    constructor() {
+  constructor() {
+    this.count = 0;
+    this.cur = "x";
+    this.field = Array.from({ length: 3 }, () => new Array(3).fill(null));
+  }
 
+  getCurrentPlayerSymbol() {
+    return this.cur;
+  }
+
+  nextTurn(rowIndex, columnIndex) {
+    if (
+      this.field[rowIndex][columnIndex] !== "x" &&
+      this.field[rowIndex][columnIndex] !== "o"
+    ) {
+      this.field[rowIndex][columnIndex] = this.cur;
+      switch (this.cur) {
+        case "x":
+          this.cur = "o";
+          break;
+        case "o":
+          this.cur = "x";
+          break;
+        default:
+          this.cur = "x";
+      }
+      this.count++;
     }
+  }
 
-    getCurrentPlayerSymbol() {
+  isFinished() {
+    return !!this.getWinner() || this.isDraw();
+  }
 
+  getWinner() {
+    //for row arrays
+    for (let i = 0; i < this.field.length; i++) {
+      if (this.field[i].every((el) => el === "x")) return "x";
+      if (this.field[i].every((el) => el === "o")) return "o";
     }
-
-    nextTurn(rowIndex, columnIndex) {
-
+    // for col arrays
+    for (let j = 0; j < this.field.length; j++) {
+      let rowArray = [];
+      for (let k = 0; k < this.field.length; k++) {
+        rowArray.push(this.field[k][j]);
+      }
+      if (rowArray.every((el) => el === "x")) return "x";
+      if (rowArray.every((el) => el === "o")) return "o";
     }
-
-    isFinished() {
-
+    // for diagonal arrays
+    let rightDiagArray = [];
+    let revDiagArray = [];
+    for (let l = 0; l < this.field.length; l++) {
+      rightDiagArray.push(this.field[l][l]);
+      revDiagArray.push(this.field[l][2 - l]);
     }
+    if (rightDiagArray.every((el) => el === "x")) return "x";
+    if (rightDiagArray.every((el) => el === "o")) return "o";
+    if (revDiagArray.every((el) => el === "x")) return "x";
+    if (revDiagArray.every((el) => el === "o")) return "o";
+    return null;
+  }
 
-    getWinner() {
+  noMoreTurns() {
+    return this.count === 9;
+  }
 
-    }
+  isDraw() {
+    return this.noMoreTurns() && this.getWinner() === null;
+  }
 
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+  getFieldValue(rowIndex, colIndex) {
+    return this.field[rowIndex][colIndex];
+  }
 }
 
 module.exports = TicTacToe;
